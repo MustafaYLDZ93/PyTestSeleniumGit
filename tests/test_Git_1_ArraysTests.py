3003c2f11318200efda66d0abc8fb431bc543c8a
MustafaYLDZ93 <mustafa.yldz093@gmail.com>
2024-05-27 17:19:44 +0300

    43

--- tests/test_Git_1_ArraysTests.py
+++ tests/test_Git_1_ArraysTests.py
@@ -11,7 +11,7 @@ class OturumAcmaTestleri(unittest.TestCase):
 
     def setUp(self):
         options = Options()
-        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--headless")
         options.binary_location = r"/usr/bin/google-chrome"
         self.driver = webdriver.Chrome(options=options)
         self.driver.get("https://www.saucedemo.com/v1/index.html")
