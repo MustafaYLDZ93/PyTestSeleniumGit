6d93a79b8c207e8f3c3e4a03370b73e4869bbce3
MustafaYLDZ93 <mustafa.yldz093@gmail.com>
2024-06-07 14:52:30 +0300

    54

--- tests/test_3_LoginwithFunction.py
+++ tests/test_3_LoginwithFunction.py
@@ -5,12 +5,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
     base_url = "https://www.saucedemo.com/v1/index.html"
     driver.get(base_url)
     yield driver
